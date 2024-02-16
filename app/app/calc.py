b2b6fc3770aec113bf40982673e4e46610810bda
Waiyankyaw <waihainkyaw@gmail.com>
2024-02-16 15:53:25 +0400

    configure file

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,12 +1,13 @@
 """
-Calculator Functions
-
+Calculator functions
 """
 
-def add(x,y):
-    """Add x and y return result"""
-    return x+y
 
-def substract(x,y):
-    "substract x and y return result"
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
     return y - x
--- app/app/test.py
+++ app/app/test.py
@@ -1,19 +1,22 @@
 """
-Sample Tests
+Sample tests
 """
-
 from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
+
     def test_add_numbers(self):
-        """test adding numbers together."""
-        res = calc.add(5,6)
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+
         self.assertEqual(res, 11)
 
-    def test_substract_number(self):
-        res = calc.substract(10,15)
-        self.assertEqual(res,5)
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 15)
 
+        self.assertEqual(res, 5)
