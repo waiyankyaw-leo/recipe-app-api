cf4f936f89258ac832172f653c39b3c910ce972c
Waiyankyaw <waihainkyaw@gmail.com>
2024-02-16 14:01:03 +0400

    add post gress configure

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator Functions
+
+"""
+
+def add(x,y):
+    """Add x and y return result"""
+    return x+y
+
+def substract(x,y):
+    "substract x and y return result"
+    return y - x
